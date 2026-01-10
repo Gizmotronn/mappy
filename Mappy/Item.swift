42cfb580695c7987f97cca93ddfe935147bf60ef
Gizmotronn <liam@skinetics.tech>
2026-01-10 14:18:52 +0800

    Initial Commit

---
+++ Mappy/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Mappy
+//
+//  Created by Liam Arbuckle on 10/1/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MappyTests/MappyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MappyTests.swift
+//  MappyTests
+//
+//  Created by Liam Arbuckle on 10/1/2026.
+//
+
+import Testing
+@testable import Mappy
+
+struct MappyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
